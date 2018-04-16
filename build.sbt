7b9ebc5bcc0fc1e65c50650a1862256fd90002c7
Shubham Jain <shubham.jain@afourtech.com>
2018-04-16 18:09:42 +0530

    Added Basic Post routes.

--- build.sbt
+++ build.sbt
@@ -23,6 +23,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.0.10",
   "com.typesafe.akka" %% "akka-http-testkit" % "10.0.10" % Test,
   "io.spray" %% "spray-routing" % sprayVersion,
-  "io.spray" %% "spray-can" % sprayVersion
+  "io.spray" %% "spray-can" % sprayVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.10"
 )
 
--- src/main/scala/sampleRouting/ServiceRoute.scala
+++ src/main/scala/sampleRouting/ServiceRoute.scala
@@ -6,10 +6,13 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import scala.io.StdIn
+import spray.json.DefaultJsonProtocol._
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
 
 
-
-trait ServiceRoute extends App  {
+trait ServiceRoute extends SprayJsonSupport with DefaultJsonProtocol  {
 
   implicit val system1 = ActorSystem("my-system")
 
@@ -17,12 +20,33 @@ trait ServiceRoute extends App  {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system1.dispatcher
 
+  final case class Person(name : String)
+
+  implicit val personFormat = jsonFormat1(Person)
+
   val route =
-    path("hello") {
+    path("hello") {  // Basic Get Request
       get {
         complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
       }
-    }
+    } ~ // Accepting Input as JSON Object
+      post {
+        path("addPerson") {
+          entity(as[Person]) { person =>
+            complete(s"Person Name is ${person.name}")
+          }
+        }
+      } ~ // Accepting Input as Form Data
+      post {
+        path("asFormData") {
+          entity(as[FormData]) { per =>
+            formFields('name,'password){
+              (name,password) =>
+                complete(s"Person Name is $name, password is $password ")
+            }
+          }
+        }
+      }
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
