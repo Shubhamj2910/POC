79d804380bc99a1d05bc5d03fe2f57539def6768
Shubham Jain <shubham.jain@afourtech.com>
2018-04-16 14:58:10 +0530

    Basic Akka-Http project setup

--- src/main/scala/sampleRouting/ServiceRoute.scala
+++ src/main/scala/sampleRouting/ServiceRoute.scala
@@ -1,20 +1,32 @@
 package sampleRouting
 
 import akka.actor.ActorSystem
-import spray.routing.SimpleRoutingApp
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
 
-trait ServiceRoute extends App with SimpleRoutingApp {
+
+trait ServiceRoute extends App  {
 
   implicit val system1 = ActorSystem("my-system")
 
-  startServer(interface = "0.0.0.0", port = 8080) {
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system1.dispatcher
+
+  val route =
     path("hello") {
       get {
-        complete {
-          <h1>This is Mumbai Server</h1>
-        }
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
       }
     }
-  }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+
+
 }
